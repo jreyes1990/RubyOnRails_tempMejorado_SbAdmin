5934800d532c768aa834c1ac22dacf4ab453368c
jreyes1990 <jarl1990mt@gmail.com>
2022-08-19 18:37:27 -0600

    Helper: Se agregaron metodo de activacion de clases

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,35 @@
 module ApplicationHelper
+  def is_active_action(controller)
+    if params[:controller] == controller
+      "collapse-item active"
+    else
+      "collapse-item"
+    end
+  end
+
+  def is_home_active
+    if params[:controller] == "home"
+      "nav-item active"
+    else
+      "nav-item"
+    end
+  end
+
+  def is_active_controller_configuracion
+    @controladores = ["usuarios", "roles", "personas_areas", "menus", "opciones", "menu_roles", "atributos", "componentes", "opcion_cas", "persona_empresa_formularios", "empresas", "areas"]
+    if @controladores.include? params[:controller]
+      "nav-link"
+    else
+      "nav-link collapsed"
+    end
+  end
+
+  def is_active_option_controller_configuracion
+    @controladores = ["usuarios", "roles", "personas_areas", "menus", "opciones", "menu_roles", "atributos", "componentes", "opcion_cas", "persona_empresa_formularios", "empresas", "areas"]
+    if @controladores.include? params[:controller]
+      "collapse show"
+    else
+      "collapse"
+    end
+  end
 end
