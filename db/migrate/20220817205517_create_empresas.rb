2eaf9fca5c445d341caeb45f3b9b2d4881f25ec3
jreyes1990 <jarl1990mt@gmail.com>
2022-08-19 22:27:27 -0600

    Se creo scaffold de empresas

---
+++ db/migrate/20220817205517_create_empresas.rb
@@ -0,0 +1,14 @@
+class CreateEmpresas < ActiveRecord::Migration[6.0]
+  def change
+    create_table :empresas do |t|
+      t.string :nombre, limit: 200
+      t.text :descripcion
+      t.integer :codigo_empresa
+      t.integer :user_created_id
+      t.integer :user_updated_id
+      t.string :estado, limit: 10
+
+      t.timestamps
+    end
+  end
+end
