f53fe7c1b8d7dd529caae652f7aabedc360f8d23
jreyes1990 <jarl1990mt@gmail.com>
2022-08-21 01:41:33 -0600

    Se creo scaffold de Roles

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,9 +1,22 @@
+# == Schema Information
+#
+# Table name: roles
+#
+#  id              :bigint           not null, primary key
+#  nombre          :string(200)
+#  descripcion     :text
+#  user_created_id :integer
+#  user_updated_id :integer
+#  estado          :string(10)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class RolesController < ApplicationController
   before_action :set_rol, only: %i[ show edit update destroy ]
 
   # GET /roles or /roles.json
   def index
-    @roles = Rol.all
+    @roles = Rol.where(:estado => 'A').order("id DESC")
   end
 
   # GET /roles/1 or /roles/1.json
@@ -22,13 +35,15 @@ class RolesController < ApplicationController
   # POST /roles or /roles.json
   def create
     @rol = Rol.new(rol_params)
+    @rol.estado = "A"
+    @rol.user_created_id = current_user.id
 
     respond_to do |format|
       if @rol.save
-        format.html { redirect_to rol_url(@rol), notice: "Rol was successfully created." }
+        format.html { redirect_to roles_url(@rol), notice: "El rol #{@rol.nombre.upcase} se ha creado correctamente." }
         format.json { render :show, status: :created, location: @rol }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html { render :new, status: :unprocessable_entity, alert: "Ocurrio un error al crear el rol, verifique!!." }
         format.json { render json: @rol.errors, status: :unprocessable_entity }
       end
     end
@@ -36,12 +51,14 @@ class RolesController < ApplicationController
 
   # PATCH/PUT /roles/1 or /roles/1.json
   def update
+    @rol.user_updated_id = current_user.id
+
     respond_to do |format|
       if @rol.update(rol_params)
-        format.html { redirect_to rol_url(@rol), notice: "Rol was successfully updated." }
+        format.html { redirect_to roles_url(@rol), notice: "El rol #{@rol.nombre.upcase} se ha actualizado correctamente." }
         format.json { render :show, status: :ok, location: @rol }
       else
-        format.html { render :edit, status: :unprocessable_entity }
+        format.html { render :edit, status: :unprocessable_entity, alert: "Ocurrio un error al actualizar el rol, verifique!!." }
         format.json { render json: @rol.errors, status: :unprocessable_entity }
       end
     end
@@ -52,8 +69,25 @@ class RolesController < ApplicationController
     @rol.destroy
 
     respond_to do |format|
-      format.html { redirect_to roles_url, notice: "Rol was successfully destroyed." }
+      format.html { redirect_to roles_url, notice: "El rol #{@rol.nombre.upcase} se ha eliminado correctamente." }
       format.json { head :no_content }
+    end
+  end
+
+  # Inactivar area
+  def inactivar_rol
+    @rol = Rol.find(params[:id])
+    @rol.user_updated_id = current_user.id
+    @rol.estado = "I"
+
+    respond_to do |format|
+      if @rol.save
+        format.html { redirect_to roles_url, notice: "El rol #{@rol.nombre.upcase} ha sido inactivada." }
+        format.json { render :show, status: :created, location: @rol }
+      else
+        format.html { render :new, status: :unprocessable_entity, alert: "Ocurrio un error al inactivar el rol, verifique!!." }
+        format.json { render json: @rol.errors, status: :unprocessable_entity }
+      end
     end
   end
 
--- test/models/rol_test.rb
+++ test/models/rol_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: roles
+#
+#  id              :bigint           not null, primary key
+#  nombre          :string(200)
+#  descripcion     :text
+#  user_created_id :integer
+#  user_updated_id :integer
+#  estado          :string(10)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 require 'test_helper'
 
 class RolTest < ActiveSupport::TestCase
