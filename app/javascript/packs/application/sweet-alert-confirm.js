3a5ad4475ddfeced0aa7afe66414a55465d46b1e
jreyes1990 <jarl1990mt@gmail.com>
2022-08-21 02:07:51 -0600

    Se debe mejorar la promesa sweetalert2

---
+++ app/javascript/packs/application/sweet-alert-confirm.js
@@ -0,0 +1,67 @@
+import Swal from 'sweetalert2/dist/sweetalert2.js'
+
+window.Swal = Swal;
+
+// Behavior after click to confirm button
+const confirmed = (element, result) => {
+    if (result.value) {
+        if (!!element.getAttribute('data-remote')) {
+            let reloadAfterSuccess = !!element.getAttribute('data-reload');
+
+            $.ajax({
+                method: element.getAttribute('data-method') || 'GET',
+                url: element.getAttribute('href'),
+                dataType: 'json',
+                success: function(result) {
+                    Swal.fire('Success!', result.message || '', 'success')
+                        .then((_result) => {
+                            if (reloadAfterSuccess) {
+                                window.location.reload();
+                            }
+                        });
+                },
+                error: function(xhr) {
+                    let title   = 'Error!';
+                    let message = 'Something went wrong. Please try again later.';
+
+                    if (xhr.responseJSON && xhr.responseJSON.message) {
+                        message = xhr.responseJSON.message;
+                    }
+
+                    Swal.fire(title, message,'error');
+                }
+            });
+        } else {
+            element.removeAttribute('data-confirm-swal');
+            element.click();
+        }
+    }
+};
+
+// Behavior after click to confirm button
+/*
+const confirmed = (element, result) => {
+};
+*/
+
+// Display the confirmation dialog
+const showConfirmationDialog = (element) => {
+    console.log('Hello world!');
+};
+
+const allowAction = (element) => {
+    if (element.getAttribute('data-confirm-swal') === null) {
+        return true;
+    }
+
+    showConfirmationDialog(element);
+    return false;
+};
+
+function handleConfirm(element) {
+    if (!allowAction(this)) {
+        Rails.stopEverything(element);
+    }
+}
+
+Rails.delegate(document, 'a[data-confirm-swal]', 'click', handleConfirm);
