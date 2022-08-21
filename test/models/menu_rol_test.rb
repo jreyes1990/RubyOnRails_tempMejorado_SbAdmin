a9da77ff3eb7ac0e088d0a6da154107737ecfb2a
jreyes1990 <jarl1990mt@gmail.com>
2022-08-21 01:49:50 -0600

    Se creo scaffold de Menu - Roles

--- test/models/menu_rol_test.rb
+++ test/models/menu_rol_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: menu_roles
+#
+#  id              :bigint           not null, primary key
+#  menu_padre      :integer
+#  user_created_id :integer
+#  user_updated_id :integer
+#  estado          :string(10)
+#  rol_id          :bigint           not null
+#  opcion_id       :bigint           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 require 'test_helper'
 
 class MenuRolTest < ActiveSupport::TestCase
