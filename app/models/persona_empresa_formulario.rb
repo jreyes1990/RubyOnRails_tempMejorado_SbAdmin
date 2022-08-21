051a0e3cf0704b9174aadadfe48258e2550e0d94
jreyes1990 <jarl1990mt@gmail.com>
2022-08-21 01:39:34 -0600

    Se creo scaffold de Persona - Empresa - Formulario

--- app/models/persona_empresa_formulario.rb
+++ app/models/persona_empresa_formulario.rb
@@ -1,4 +1,20 @@
+# == Schema Information
+#
+# Table name: persona_empresa_formularios
+#
+#  id               :bigint           not null, primary key
+#  descripcion      :text
+#  user_created_id  :integer
+#  user_updated_id  :integer
+#  estado           :string(10)
+#  opcion_ca_id     :bigint           not null
+#  personas_area_id :bigint           not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
 class PersonaEmpresaFormulario < ApplicationRecord
   belongs_to :opcion_ca
   belongs_to :personas_area
+
+  validates_uniqueness_of :personas_area_id, :scope => :opcion_ca_id, :message => " error!! solo puede asignar una vez el permiso"
 end
--- test/models/persona_empresa_formulario_test.rb
+++ test/models/persona_empresa_formulario_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: persona_empresa_formularios
+#
+#  id               :bigint           not null, primary key
+#  descripcion      :text
+#  user_created_id  :integer
+#  user_updated_id  :integer
+#  estado           :string(10)
+#  opcion_ca_id     :bigint           not null
+#  personas_area_id :bigint           not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
 require 'test_helper'
 
 class PersonaEmpresaFormularioTest < ActiveSupport::TestCase
