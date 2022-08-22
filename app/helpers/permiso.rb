8c07e5feebe99366dd57ab3f7f36866566829faa
jreyes1990 <jarl1990mt@gmail.com>
2022-08-21 18:23:12 -0600

    Herlper de permiso

---
+++ app/helpers/permiso.rb
@@ -0,0 +1,14 @@
+class Permiso
+  def initialize(componente, atributo)
+    @componente = componente
+    @atributo = atributo
+  end
+
+  def componente
+    @componente
+  end
+
+  def atributo
+    @atributo
+  end
+end
