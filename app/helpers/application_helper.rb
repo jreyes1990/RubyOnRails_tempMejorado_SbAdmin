c382dd519aad77e632235233db055f36e731a855
jreyes1990 <jarl1990mt@gmail.com>
2022-08-21 18:22:43 -0600

    Se agrego metodos de usuario y configuracion

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,18 @@
 module ApplicationHelper
+
+  def current_user_name
+    persona = Persona.where("user_id = ? ", current_user.id).first
+    if (persona != nil) then
+      if (persona.nombre != nil || persona.apellido != nil)
+        return persona.nombre.upcase + " " + persona.apellido.upcase
+      else
+        return current_user.email
+      end
+    else
+      return current_user.email
+    end
+  end
+
   def is_active_action(controller)
     if params[:controller] == controller
       "collapse-item active"
@@ -9,6 +23,15 @@ module ApplicationHelper
 
   def is_home_active
     if params[:controller] == "home"
+      "nav-item active"
+    else
+      "nav-item"
+    end
+  end
+
+  def is_active_controller_configuracion_titulo
+    @controladores = ["usuarios", "roles", "personas_areas", "menus", "opciones", "menu_roles", "atributos", "componentes", "opcion_cas", "persona_empresa_formularios", "empresas", "areas"]
+    if @controladores.include? params[:controller]
       "nav-item active"
     else
       "nav-item"
